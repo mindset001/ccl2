eb7da9f3fbbd04a8cc471e491bd5da0082733776
shakiru102 <ashakiru53@gmail.com>
2024-07-14 14:08:31 +0100

    color

--- src/app/components/work.tsx
+++ src/app/components/work.tsx
@@ -43,10 +43,10 @@ function Work() {
            <h1 className="mb-2 text-center  text-4xl font-[500] text-white  mt-6 capitalize">Why work with us</h1>
            <div className='lg:p-8'>
 
-           <p>Capacity Connect is a Nigeria based consulting firm committed to enhancing capacity and fostering connections for sustainable development globally and especially in developing countries. We have deep local knowledge and expansive global view and network that provides the right setting for local solutions informed by global best practices. We know what local and global stakeholders require from each other and are well positioned to bridge the divide effectively, respectfully and with maximum impact. </p>
-            <p>Our project management and social sectoral expertise coupled with effective inclusive approaches, access to extensive resources across a diverse sector, incredible passion for social impact and a deep culture of service and professionalism qualifies us to be your partner for delivering sustainable development and humanitarian interventions. </p>
-            <p>We provide you and your partners access to in-house experts and a network of local and international professionals to get you the results you need, enhance capacities, and foster lifelong connections for sustainable development.</p>
-            <p>We emphasis integrity and collaboration in every stage of our engagement, ensuring that all your requirements are prioritized, and all your expectations are met and exceeded. </p>
+           <p className='text-[#fff]'>Capacity Connect is a Nigeria based consulting firm committed to enhancing capacity and fostering connections for sustainable development globally and especially in developing countries. We have deep local knowledge and expansive global view and network that provides the right setting for local solutions informed by global best practices. We know what local and global stakeholders require from each other and are well positioned to bridge the divide effectively, respectfully and with maximum impact. </p>
+            <p className='text-[#fff]'>Our project management and social sectoral expertise coupled with effective inclusive approaches, access to extensive resources across a diverse sector, incredible passion for social impact and a deep culture of service and professionalism qualifies us to be your partner for delivering sustainable development and humanitarian interventions. </p>
+            <p className='text-[#fff]'>We provide you and your partners access to in-house experts and a network of local and international professionals to get you the results you need, enhance capacities, and foster lifelong connections for sustainable development.</p>
+            <p className='text-[#fff]'>We emphasis integrity and collaboration in every stage of our engagement, ensuring that all your requirements are prioritized, and all your expectations are met and exceeded. </p>
        
            </div>
            </div>
@@ -55,9 +55,9 @@ function Work() {
       <h1 className="text-center text-black text-4xl font-bold capitalize">Our Approach- the ABCDE +S Approach</h1>
      <div className='grid lg:grid-cols-3 gap-4 mt-4'>
      {slides.map((slide, index) => (
-        <div className=' bg-[#0E1729] rounded-[20px] p-4'>
+        <div className=' bg-[#0E1729] rounded-[20px] p-4 '>
             {/* <h2>{slide .title}</h2> */}
-            <p>{slide.content}</p>
+            <p className='text-[#fff]'>{slide.content}</p>
         </div>
      ))}
      </div>
