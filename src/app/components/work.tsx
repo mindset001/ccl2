b678057801cdeb31310e8771f6820cc4632465f2
shakiru102 <ashakiru53@gmail.com>
2024-07-14 08:08:14 +0100

    mobile

--- src/app/components/work.tsx
+++ src/app/components/work.tsx
@@ -35,11 +35,11 @@ function Work() {
     <div  className="w-[90%] flex flex-col items-center">
     
 
-    <div className='flex flex-col '>
-        <div className=' w-full flex justify-between items-center'>
+    <div className='flex flex-col items-center justify-center '>
+        <div className='hidden  w-full lg:flex flex-col lg:flex-row justify-between items-center'>
         <h1 className="w-1/3 text-align text-black text-[80px] font-[500] leading-[90px] mt-10 capitalize">Why work <br /> with us</h1>
 
-           <div className='w-[70%] text-justify bg-[#0E1729]  p-10 mt-2 rounded-[20px] flex flex-col gap-2'>
+           <div className='lg:w-[70%] text-justify bg-[#0E1729]  p-10 mt-2 rounded-[20px] flex flex-col gap-2'>
            <p>Capacity Connect is a Nigeria based consulting firm committed to enhancing capacity and fostering connections for sustainable development globally and especially in developing countries. We have deep local knowledge and expansive global view and network that provides the right setting for local solutions informed by global best practices. We know what local and global stakeholders require from each other and are well positioned to bridge the divide effectively, respectfully and with maximum impact. </p>
             <p>Our project management and social sectoral expertise coupled with effective inclusive approaches, access to extensive resources across a diverse sector, incredible passion for social impact and a deep culture of service and professionalism qualifies us to be your partner for delivering sustainable development and humanitarian interventions. </p>
             <p>We provide you and your partners access to in-house experts and a network of local and international professionals to get you the results you need, enhance capacities, and foster lifelong connections for sustainable development.</p>
@@ -47,9 +47,9 @@ function Work() {
        
            </div>
         </div>
-        <div className='w-full mt-20 '>
+        <div className='w-[80%] lg:w-full mt-20 '>
       <h1 className="text-center text-black text-4xl font-bold capitalize">Our Approach- the ABCDE +S Approach</h1>
-     <div className='grid grid-cols-3 gap-4 mt-4'>
+     <div className='grid lg:grid-cols-3 gap-4 mt-4'>
      {slides.map((slide, index) => (
         <div className=' bg-[#0E1729] rounded-[20px] p-4'>
             {/* <h2>{slide .title}</h2> */}
