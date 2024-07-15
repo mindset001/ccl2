a42dbde1e503cf6a1fda830fd74b1d473e0eb26f
shakiru102 <ashakiru53@gmail.com>
2024-07-15 10:19:23 +0100

    summary

--- src/app/components/newTeam2.tsx
+++ src/app/components/newTeam2.tsx
@@ -32,85 +32,88 @@ interface Tab {
 
 export default function NewTeam2() {
 
-  const tabs: Tab[] = [
-    {
-      name: "Amina Bala Zakari",
-      title: "Chair Board of Directors",
-      img: TabImage01,
-      description: "Amina is a pharmacist and a health and social sector development expert with a wealth of experience...",
-      link: ''
-    },
-    {
-      name: 'Olajumoke Azogu,',
-      title: "Member Board of Directors",
-      description: "Jumoke holds a Ph.D. (Information Science) with about 15 years experience in Monitoring and Evaluation of Public Health programs...",
-      img: TabImage2,
-      link: ''
-    },
-    {
-      name: "Kole Shettima PhD",
-      title: "Member Board of Directors",
-      description: "Kole Shettima is the African Regional Director of the MacArthur Foundation's and Co-Director of the On Nigeria Big Bet...",
-      img: TabImage3,
-      link: ''
-    },
-    {
-      name: "Dr Yashua Alkali Hamza PhD.",
-      title: "Member Board of Directors",
-      description: "Yashua is a Pediatrician and Public Health expert with vast experience in the areas of Public Health Policy, Qualitative Research and Hospital Administration...",
-      img: TabImage4,
-      link: ''
-    },
-    {
-      name: 'Amina Ahmad K.U Nagogo',
-      title: "Member Board of Directors",
-      description: "Results-driven social entrepreneur with over 10 years working experience in development and management of digital health initiatives...",
-      img: TabImage5,
-      link: ''
-    },
-    {
-      name: 'Amina Baba Manu',
-      title: "Member Board of Directors",
-      description: "Amina Baba Manu has over 20 years of working in the development sector...",
-      img: TabImage6,
-      link: ''
-    },
-    {
-      name: 'Yakubu Jibrin',
-      title: "Member Board of Directors",
-      description: "Yakubu Jibrin, MBA, BSc (Hons) Accounting, FIMC is a Finance, Human Resource, and Operations Management specialist with about 30 years’ experience...",
-      img: TabImage7,
-      link: ''
-    },
-    {
-      name: 'Mohammad Farouk Jega',
-      title: "Member Board of Directors, Senior Program Director",
-      description: "Farouk is an obstetrics and gynecologist and project management expert with over 2 decades of clinical, managerial and leadership roles in the health and development sector...",
-      img: TabImage8,
-      link: ''
-    },
-    {
-      name: 'Fatima Jika',
-      title: "Member Board of Directors",
-      description: "Seasoned and accomplished financial professional with a strong background in risk management, regulatory leadership, and strategic planning...",
-      img: TabImage9,
-      link: ''
-    },
-    {
-      name: 'Mike Egboh',
-      title: "Member Board of Directors",
-      description: "A notable figure in the development sector in Nigeria and beyond with over 4 decades of leadership roles in the non-profit, global health and development sector...",
-      img: TabImage10,
-      link: ''
-    },
-    {
-      name: 'Mohammad Murtala Mai',
-      title: "CEO/Secretary, Board of Directors",
-      description: "Experienced and strategic, international development leader with almost 3 decades of success in managing complex, expansive, global, health and development initiatives...",
-      img: TabImage11,
-      link: ''
-    }
-  ]
+    const tabs: Tab[] = [
+        {
+            name: "Amina Bala Zakari",
+            title: "Chair Board of Directors ",
+            img: TabImage01,
+            description: "Amina is a pharmacist and health and social sector development expert with extensive experience in policy and strategic development, program design, management, and evaluation. She has 10 years of experience at the Independent National Electoral Commission, where she served as national commissioner in various capacities, including acting Chairperson. She has also held roles such as Secretary of Health and Human Services and Secretary of Social Development in the Federal Capital Territory Administration. Additionally, she was a Senior Consultant at Afri-project Consortium. Amina serves on several boards, including the Albino Foundation of Nigeria and Reach Care Multi-Services Nigeria Ltd.",
+            link: ''
+        },
+          {
+              name: 'Olajumoke Azogu,',
+              title: "Member Board of Directors",
+              description: "Jumoke holds a Ph.D. (Information Science) with about 15 years experience in Monitoring and Evaluation of Public Health programs, 9 years of which involve technical and team leadership. Currently the MEL Director with Palladium International for the USAID Integrated Health Program (IHP), a $225M USAID Nigeria-funded Reproductive, Maternal, Newborn, Child Health and Nutrition, and Malaria (RMNCH+NM) program for Health Systems Strengthening and Health Financing, where she provides strategic guidance and technical leadership, vision and management, capacity strengthening for MEL activities, working with National and State partners.",
+              img: TabImage2,
+              link: ''
+          },
+          {
+              name: "Kole Shettima PhD",
+              title: "Member Board of Directors ",
+    
+              description: "Kole Shettima is the African Regional Director of the MacArthur Foundation and Co-Director of the On Nigeria Big Bet. He has managed grantmaking in various sectors, including Population & Reproductive Health and Human Rights. Before joining the Foundation in 1999, Shettima taught at several universities and volunteered for numerous organizations. He has published in various academic journals and holds a PhD from the University of Toronto, a Master's from Ahmadu Bello University, and an undergraduate degree from the University of Maiduguri. He serves on the boards of several organizations, including the JAIZ Charity and Development Foundation and the Sir Ahmadu Bello Memorial Foundation",
+              img: TabImage3,
+              link: ''
+          },
+          {
+              name: "Dr Yashua Alkali Hamza PhD. ",
+              title: "Member Board of Directors ",
+    
+              description: "Yashua is a Pediatrician and Public Health expert with extensive experience in Public Health Policy, Qualitative Research, and Hospital Administration. She collaborates with various agencies and institutions, including Harvard School of Public Health and NYU RCLA. She is the chief medical editor of the Holistic and Wellness website and co-founder of Family Included. Yashua has led numerous projects to improve women's and children's health, and she offers life coaching and therapy at her private facility. She holds a Master of Public Health from the University of Liverpool and a Ph.D. in Public Health from Walden University. Yashua is the Founder and CEO of Childcare and Wellness Clinics in Abuja, Nigeria.",
+              img: TabImage4,
+              link: ''
+          },
+          {
+              name: 'Amina Ahmad K.U Nagogo',
+              title: "Member Board of Directors",
+              description: "Results-driven social entrepreneur with over 10 years working experience in development and management of digital health initiatives. She is the Founder and CEO of Datharm Nigeria Ltd, a company with a passion for developing tools for data driven decision making in the field of public health and education.",
+              img: TabImage5,
+              link: ''
+          },
+          {
+              name: 'Amina Baba Manu ',
+              title: "Member Board of Directors",
+              description: "Amina Baba Manu has over 20 years of experience in the development sector, working with organizations like STEP and ActionAid Nigeria in various capacities. She has supported over 500 NGOs with technical assistance, skill development, and mentoring in areas such as organizational development, fundraising, and strategic planning. Currently an independent consultant, Amina collaborates with multiple organizations, including UNICEF and Save the Children. She is dedicated to helping small national NGOs grow and become sustainable.",
+              img: TabImage6,
+              link: ''
+          },
+          {
+              name: 'Yakubu Jibrin ',
+              title: "Member Board of Directors",
+              description: "Yakubu Jibrin, MBA, BSc (Hons) Accounting, FIMC, is a specialist in Finance, Human Resource, and Operations Management with nearly 30 years of experience, including 10 years in management roles within the development sector. He served as Director of Business Operations at Friends for Global Health Initiative and Friends in Global Health from 2009 to 2017. He previously worked with Family Health International and Partnership for Transforming Health Systems from 2005 to 2009, and supported a Bill and Melinda Gates Foundation project in Kano. He also served as a Technical Assistant in Finance for the Presidential Task Force on COVID-19. Before entering the development sector, he held finance and administration roles at MicroAccess Limited, Afri-Projects Consortium, and New Nigeria Development Company.",
+              img: TabImage7,
+              link: ''
+          },
+          {
+              name: 'Mohammad Farouk Jega',
+              title: "Member Board of Directors, Senior Program Director",
+              description: "Farouk is an obstetrics and gynecologist and project management expert with over 2 decades of clinical, managerial and leadership roles in the health and development sector. He had served as Country Director of Pathfinder International for Nigeria and Ghana from 2012 to 2020 and Senior Program Director/Regional Director from 2020 to 2022. He holds a medical degree from the University of Jos and a Master of Public Health from Liverpool School of Tropical Medicine.",
+              img: TabImage8,
+              link: ''
+          },
+          {
+              name: 'Fatima Jika',
+              title: "Member Board of Directors",
+              description: "Seasoned and accomplished financial professional with a strong background in risk management, regulatory leadership, and strategic planning. Proven track record in implementing diverse projects, ensuring regulatory compliance, and collaborating effectively with international development partners, notably the Alliance for Financial Inclusion (AFI). Backed by an academic foundation in International Relations, with a focus on bringing a holistic perspective to the finance field and applying financial expertise to drive sustainable financial solutions and foster positive change.  Fatima holds a Ph.D. in Economics, MA in International Relations, and an MBA, among other qualifications.",
+              img: TabImage9,
+              link: ''
+          },
+          {
+              name: 'Mike Egboh',
+              title: "Member Board of Directors",
+              description: "A notable figure in the development sector in Nigeria and beyond with over 4 decades of leadership roles in the non-profit, global health and development sector. Mike was Country Director for Pathfinder International for close 2 decades. He also successfully led the PATH II project at the Project Director. More recently, he was the Country Director and Chief of Party for the Chemonics led Global Supply Chain project here in Nigeria. Mike currently mentors and coaches’ development leaders and organizations in the sub-region and globally. He holds a Master of Public Health from the Harvard University, School of public health. He is a recipient of various national and global awards including the prestigious alumni award of the Harvard School of Public Health and US president’s Volunteer Service Award.",
+              img: TabImage10,
+              link: ''
+          },
+          {
+              name: 'Mohammad Murtala Mai',
+              title: "CEO/Secretary, Board of Directors",
+              description: "Murtala is an experienced international development leader with nearly 30 years of success in managing global health and development initiatives. He spent 22 years in various leadership roles at Pathfinder International, including Country Director for Nigeria and Ghana, Senior Technical Director Best Practices, Regional Director, Chief of Country and Program Strategy, Chief of Programs, and Executive Vice President. Previously, he was a Senior Consultant at Afri-project Consortium and served as Senior Special Assistant Health to the Governor of Adamawa State. He also chaired the technical committee board of the National Primary Healthcare Development Agency. Murtala holds a medical degree from Ahmadu Bello University, an MPH from the University of Maastricht, and an MBA from the Frankfurt School of Business and Finance.",
+              img: TabImage11,
+              link: ''
+          },
+        
+      ]
 
   return (
     <main className='mt-10 flex flex-col justify-center items-center'>
